a2a2dd65040ca218ac401b967c71a378e1205db9
Marchelloo <49792565+markodjunev@users.noreply.github.com>
2021-02-06 20:10:20 +0200

    Create Cat Server Integration

--- Server/Catstagram.Server/Catstagram.Server/Data/Models/User.cs
+++ Server/Catstagram.Server/Catstagram.Server/Data/Models/User.cs
@@ -1,8 +1,10 @@
 ﻿namespace Catstagram.Server.Data.Models
 {
     using Microsoft.AspNetCore.Identity;
+    using System.Collections.Generic;
 
     public class User : IdentityUser
     {
+        public IEnumerable<Cat> Cats { get; } = new HashSet<Cat>();
     }
 }
---
+++ Server/Catstagram.Server/Catstagram.Server/Models/Cats/CreateCatRequestModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Catstagram.Server.Models.Cats
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CreateCatRequestModel
+    {
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [MaxLength(2000)]
+        public string Description { get; set; }
+    }
+}
